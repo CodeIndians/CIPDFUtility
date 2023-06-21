ee30b5bb135d1d8caf66cd9b0a4bc962d27bb3e2
Raghuveer Ch S <raghuveerchs@gmail.com>
2023-06-21 19:16:44 +0530

    Added a save function to the Utility class

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ output_file = "output.pdf"
 
 pdf_bookmark_adder = PDFUtility(input_file, output_file,(0,0,1))
 pdf_bookmark_adder.add_bookmarks_from_text("Finished Opening")
+pdf_bookmark_adder.save_pdf_file()
--- pdf_util.py
+++ pdf_util.py
@@ -2,23 +2,27 @@ import fitz
 
 class PDFUtility:
     def __init__(self, input_file, output_file,color):
-        self.input_file = input_file
         self.output_file = output_file
         self.color = color
+        # Open the input PDF
+        self.my_pdf = fitz.open(input_file)
     
     def add_bookmarks_from_text(self, search_string):
-        # Open the input PDF
-        my_pdf = fitz.open(self.input_file)
+        
+        # navigate through all the pages
+        for n_page in self.my_pdf: 
+            # search for will return the rects
+            matchWordQuads = n_page.search_for(search_string)
 
-        for n_page in my_pdf:  
-            matchWords = n_page.search_for(search_string)      
-            for word in matchWords:  
-                annot = n_page.add_rect_annot(word)
+            # Add annotations with the specified color for all the acquired rects  
+            for wordQuad in matchWordQuads:  
+                annot = n_page.add_rect_annot(wordQuad)
                 annot.set_border(width=0)
                 annot.set_colors(stroke=self.color, fill=self.color)
                 annot.set_opacity(0.3)
                 annot.update() 
 
-        # Save the modified PDF with bookmarks
-        my_pdf.save(self.output_file)
-        print(f"Bookmarks added to '{self.input_file}'. Saved as '{self.output_file}'.")
+    def save_pdf_file(self):
+        # Save the modified PDF with bookmarks and annotations to a new
+        self.my_pdf.save(self.output_file)
+        print(f"Bookmarks added and Saved as '{self.output_file}'.")
