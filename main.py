223beeed1581b9e10222fa4ccb6adda5be05d986
Raghuveer Ch S <raghuveerchs@gmail.com>
2023-06-22 09:22:35 +0530

    Added installer ignore files

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ search_texts = [" O.C. ", " Shear Wall ", " On-center ", " Panel ", " Roof ", " 
 # search_texts = [" O.C. "]
 
 pdf_bookmark_adder = PDFUtility(input_file, output_file,(0,0,1))
+
+# loop through the search text and call bookmark adder function on each of the search string
 for text in search_texts:
     # print(text)
     pdf_bookmark_adder.add_bookmarks_from_text(text)
